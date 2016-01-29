7eed12cacb73f549972823748eaee14644ef89b8
Chamlee <arnaud8ruckebusch@gmail.com>
2016-01-29 07:40:52 +0100

    add angular apps

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chamleeAngularCouchdbApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chamleeAngularCouchdbApp
+ */
+angular.module('chamleeAngularCouchdbApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
