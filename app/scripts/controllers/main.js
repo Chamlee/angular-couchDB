484467abd0823b3d4316462e1db041211e88cd80
Chamlee <arnaud8ruckebusch@gmail.com>
2016-01-31 18:23:21 +0100

    simpe form + post on couchDB

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,18 +9,40 @@
  */
 angular.module('chamleeAngularCouchdbApp')
     .controller('MainCtrl', function ($scope, $http, appSettings) {
-        console.log(appSettings);
-        this.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-
-        function getItems () {
+        function getItems() {
             $http.get(appSettings.db + '/_design/app/_view/byName')
                 .success(function (data) {
+                    console.log(data.rows);
                     $scope.items = data.rows;
                 });
         }
         getItems();
+
+        $scope.name = null;
+        $scope.price = null;
+        $scope.items = [];
+        $scope.status = '';
+
+        $scope.processForm = function () {
+            var item = {
+                key: $scope.name,
+                value: $scope.price
+            };
+            postItem(item);
+        };
+
+        function postItem(item) {
+            console.log(item);
+            // optimistic ui update
+            $scope.items.push({key: $scope.name, value: $scope.price});
+            console.log($scope.items);
+            // send post request
+            $http.post(appSettings.db, item)
+                .success(function () {
+                    //getItems();
+                }).error(function (res) {
+                $scope.status = 'Error: ' + res.reason;
+                // refetch items from server
+            });
+        }
     });
